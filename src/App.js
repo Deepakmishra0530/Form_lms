d84320e8e062316486c3de95649f66676430502f
Deepakmishra0530 <dm030843@gmail.com>
2024-06-08 14:46:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import { useState } from 'react'
-import logo from './logo.svg';
+
+import { Routes,Route } from 'react-router-dom';
+import Form1 from './components/Form2';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Form1 from './components/Form1';
 
 function App() {
 
-  
 
- 
-
- 
-
- 
   return (
     <>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Form1} />
-        {/* <Route path="/success" component={Success} /> */}
-      </Switch>
-    </Router>
+    <Routes>
+
+      <Route path='/' element={
+        <div className='relative'>
+
+          <Form1/>
+        </div>
+        
+        
+        } />
+      <Route path='success' element={
+        <div className='text-center mt-96 text-3xl'>
+         Successfully Submitted !
+        </div>
+      } />
+
+
+    </Routes>
+     
     </>
   );
 };
